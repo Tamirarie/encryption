d49fce7aa2d6c4043518a014123d25201a5df911
Tamir <Tamir@Tamir-PC>
2017-08-09 19:04:30 +0300

    update

---
+++ maven-encryption/src/main/java/menu.java
@@ -0,0 +1,10 @@
+
+public class menu {
+	public static void main(String[] args) {
+		System.out.println("Welcome to our program!\nPlease Enter number"
+				+ "to Choose method:\n"
+				+"1: Encryption\n2: Decryption\n"
+				);
+		
+	}
+}
